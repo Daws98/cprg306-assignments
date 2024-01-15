d3730f16b0a94e71335f78ffa27a0672b1736039
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-15 13:39:06 -0700

    Finished Week 2
    final touch ups

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
+import Link from 'next/link'
 export default function Page() {
-  return <main>Hello World!</main>
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li>
+          <Link href='week-2'>Week 2</Link>
+        </li>
+      </ul>
+    </main>
+    
+  )
+  
+  
 }
