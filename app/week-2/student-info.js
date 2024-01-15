3ac9eca16ae4aac60e93249015683efc87b6268b
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-15 13:22:04 -0700

    Student Info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+export function StudentInfo() {
+    return (
+      <div>
+          <p>Dawson Gall</p>
+          <p>
+              <a href="https://github.com/Daws98">https://github.com</a>
+          </p>
+      </div>
+    );
+  }
