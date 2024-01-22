5d2229cedd7c7726774470bef4ead8b5097d3a5c
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 12:49:40 -0700

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Item = ({ item }) => {
+const Item = ({ name, quantity, category }) => {
     return (
-        <li style={{ border: '1px solid lightgrey', padding: '10px', margin: '10px', listStyleType: 'none', backgroundColor: 'lightgrey', color: 'black' }}>
-            <h3 className="text-lg font-bold">{item.name}</h3>
-            <p>Quantity: {item.quantity}</p>
-            <p>Category: {item.category}</p>
+        <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black">
+            <h3 className="text-lg font-bold">{name}</h3>
+            <p>Quantity: {quantity}</p>
+            <p>Category: {category}</p>
         </li>
     );
 };
