ca8b50452193035aa4c03925b7d3f20cee512b90
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 13:32:02 -0700

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
 const Item = ({ name, quantity, category }) => {
     return (
-        <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black">
-            <input type="checkbox" className="mr-2" />
+        <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black flex items-center">
             <h3 className="text-lg font-bold capitalize">{name}</h3>
             <p>Quantity: {quantity}</p>
             <p className="capitalize">Category: {category}</p>
+            <input type="checkbox" className="ml-auto mr-2" />
         </li>
     );
 };
