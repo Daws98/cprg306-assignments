c88b4cf2f710a5d208322e2f346ec048c64bd535
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 13:31:28 -0700

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,7 @@
 const Item = ({ name, quantity, category }) => {
     return (
         <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black">
+            <input type="checkbox" className="mr-2" />
             <h3 className="text-lg font-bold capitalize">{name}</h3>
             <p>Quantity: {quantity}</p>
             <p className="capitalize">Category: {category}</p>
