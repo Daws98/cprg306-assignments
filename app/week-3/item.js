eb82477df4a0cdb8752ea2085717eab59c474fde
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 13:25:52 -0700

    Capitaliza

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 const Item = ({ name, quantity, category }) => {
     return (
         <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black">
-            <h3 className="text-lg font-bold">{name}</h3>
+            <h3 className="text-lg font-bold capitalize">{name}</h3>
             <p>Quantity: {quantity}</p>
             <p>Category: {category}</p>
         </li>
