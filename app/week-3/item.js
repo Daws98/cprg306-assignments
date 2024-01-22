90e19904c26cf24b389425704066894d0d68e33e
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 12:24:54 -0700

    tailwind changes

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = ({ name, quantity, category }) => {
     return (
-        <li className="bg-gray-200 p-4 rounded-md">
+        <li className>
             <h3 className="text-lg font-bold">{name}</h3>
             <p>Quantity: {quantity}</p>
             <p>Category: {category}</p>
