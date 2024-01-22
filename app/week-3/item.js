06438f3e5817012e830be50413e37e708c8c9a50
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 13:33:02 -0700

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,7 @@
+import React from 'react';
 const Item = ({ name, quantity, category }) => {
     return (
-        <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black flex items-center">
+        <li className="border border-gray-300 p-4 m-4 bg-gray-200 text-black flex flex-col">
             <h3 className="text-lg font-bold capitalize">{name}</h3>
             <p>Quantity: {quantity}</p>
             <p className="capitalize">Category: {category}</p>
