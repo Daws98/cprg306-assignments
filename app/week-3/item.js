50eb38c72f1ff1266d201581372e6e87d9363bb6
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 12:31:33 -0700

    style

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = ({ item }) => {
     return (
-        <li style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+        <li style={{ border: '1px light grey', padding: '10px', margin: '10px' }}>
             <h3 className="text-lg font-bold">{item.name}</h3>
             <p>Quantity: {item.quantity}</p>
             <p>Category: {item.category}</p>
