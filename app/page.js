02fb9a82a730691fe575d1d816f9a1b81763bc04
Dawson <125399990+Daws98@users.noreply.github.com>
2024-01-22 12:10:43 -0700

    Added Week 3

--- app/page.js
+++ app/page.js
@@ -8,6 +8,11 @@ export default function Page() {
           <Link href='week-2'>Week 2</Link>
         </li>
       </ul>
+      <ul>
+        <li>
+          <Link href='week-3'>Week 3</Link>
+        </li>
+      </ul>
     </main>
     
   )
