9bdc76da5d0e30cfa306f8a9f3aba0838cfc49c3
Dawson <125399990+Daws98@users.noreply.github.com>
2024-02-12 12:46:43 -0700

    finally! Group By

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -7,45 +7,65 @@ function ItemList() {
   const [itemList, setItemList] = useState(items);
   const [sortBy, setSortBy] = useState("name");
 
-  const sortedItems = [...itemList].sort((a, b) => {
+  const sortedItems = itemList.sort((a, b) => {
     if (sortBy === "name") {
       return a.name.localeCompare(b.name);
-    } else if (sortBy === "category") {
+    }
+    if (sortBy === "category") {
       return a.category.localeCompare(b.category);
     }
+    if (sortBy === 'groupedCategory') {
+      return a.category.localeCompare(b.category);
+    }
+    return a.quantity - b.quantity;
   });
 
-  const handleSortByName = () => {
-    setSortBy("name");
-  };
+  const handleGroupByCategory = () => {
+    const groupedItems = sortedItems.reduce((acc, item) => {
+      const category = item.category;
+      acc[category] = [...(acc[category] || []), item];
+      return acc;
+    }, {});
 
-  const handleSortByCategory = () => {
-    setSortBy("category");
+    return Object.entries(groupedItems).map(([category, itemsInCategory]) => (
+      <div key={category}>
+        <h2 className="text-lg font-bont mb-2 capitalize px-4">{category}</h2>
+        {itemsInCategory.map((item) => (
+          <Item key={item.id}{...item} />
+        ))}
+      </div>
+    ));
+
   };
 
   return (
     <div>
       <button
-        onClick={handleSortByName}
+        onClick={() => setSortBy('name')}
         style={{ backgroundColor: sortBy === "name" ? "grey" : "lightgrey", fontFamily: 'Indie Flower', padding: '5px' }}
       >
         Sort by Name
       </button>
       <button
-        onClick={handleSortByCategory}
+        onClick={() => setSortBy('category')}
         style={{ backgroundColor: sortBy === "category" ? "grey" : "lightgrey", fontFamily: 'Indie Flower', padding: '5px' }}
       >
         Sort by Category
       </button>
+      <button
+        onClick={() => setSortBy('groupedCategory')}
+        style={{ backgroundColor: sortBy === "groupedCategory" ? "grey" : "lightgrey", fontFamily: 'Indie Flower', padding: '5px' }}
+      >
+        Group by Category
+      </button>
+      {sortBy === 'groupedCategory' ? (
+        handleGroupByCategory()
+      ) : (
+        sortedItems.map((item) => (
+          <Item key={item.id} {...item} /> 
+        ))
+      )}
       <ul style={{ fontFamily: 'Indie Flower' }}>
-        {sortedItems.map((item) => (
-          <Item
-            key={item.id}
-            name={item.name}
-            quantity={item.quantity}
-            category={item.category}
-          />
-        ))}
       </ul>
     </div>
   );
