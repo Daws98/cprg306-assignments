b8d8a6d6548f5e070d65d494d73dbfa72f63fe17
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-04 13:36:09 -0700

    Week 6

--- app/page.js
+++ app/page.js
@@ -32,6 +32,13 @@ export default function Page() {
           </div>
         </li>
       </ul>
+      <ul>
+        <li>
+          <div className="week-link" style={{ textAlign: 'center', border: '1px solid black', padding: '10px', backgroundColor: 'lightgray', color: 'black', fontFamily: 'Indie Flower' }}>
+            <Link href='week-6' style={{ fontSize: '1.2rem' }}>Week 6</Link>
+          </div>
+        </li>
+      </ul>
     </main>
   )
 }
