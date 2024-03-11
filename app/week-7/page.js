77f52fe97ceaae4bea8bf66598cd576c16442c3e
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-11 11:54:55 -0600

    Creation of Week 7

---
+++ app/week-7/page.js
@@ -0,0 +1,16 @@
+"use client"
+import React, { useState } from 'react';
+import ItemList from './item-list.js';
+import NewItem from './new-item.js';
+
+export default function Page(){
+    const [items, setItems] = useState([]);
+
+    return (
+        <main className="container mx-auto p-4">
+            <h1 className="text-5xl font-bold mb-4 text-center" style={{ fontFamily: 'Indie Flower', color: 'lightgray' }}>Shopping List</h1>
+            <NewItem items={items} setItems={(setItems)} />
+            <ItemList items={items} />
+        </main>
+    );
+};
