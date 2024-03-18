8b304d97b0a9611d8776f562915bc16c6e190a98
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-18 16:58:08 -0600

    Final touch ups

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -15,7 +15,7 @@ return (
         {/* If user is logged in, display welcome message, logout button, and link to shopping list page */}
         {user ? (
             <div style={{ background: 'lightgray', padding: '10px', color: 'black', textAlign: 'center' }}>
-                <h1 style={{ fontSize: '24px' }}>Welcome, {user.displayName} ({user.email})</h1>
+                <h1 style={{ fontSize: '24px', fontFamily: 'Indie Flower' }}>Welcome, {user.displayName} ({user.email})</h1>
                 <div>
                     <button style={{ background: 'lightblue', padding: '5px 10px', margin: '0 auto' }} onClick={handleLogout}>Logout</button>
                     <span style={{ margin: '0 10px' }}></span>
