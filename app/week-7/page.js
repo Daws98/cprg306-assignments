c88fd0953f30d66d2cff4f37acde1ebc857778b8
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-27 17:41:55 -0600

    Finally

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,41 +1,40 @@
-'use client'
+"use client";
 import React, { useState } from 'react';
 import ItemList from './item-list.js';
 import NewItem from './new-item.js';
 import MealIdeas from './meal-ideas.js';
-import itemsData from "./items.json";
 
-export default function Page(){
-    const [items, setItems] = useState(itemsData);
-    const [selectedItemName, setSelectedItem] = useState('');
+const Page = () => {
+    const [items, setItems] = useState([]);
+    const [selectedItemName, setSelectedItemName] = useState(null);
 
-    const handleAddItem = (item) => {
-      setItems([...items, item]);
+    const handleItemSelect = (itemName) => {
+        // Clean up item name if necessary
+        const cleanedItemName = itemName.replace(/,.*|(\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF]|\uD83E[\uDD00-\uDDFF]|[\u2700-\u27BF])/g, '').trim();
+        setSelectedItemName(cleanedItemName);
     };
+    
+    
+    
+    
+    
 
-    const handleItemSelect = (id) => {
-      const selectedIngredient = items.find((ingredient) => ingredient.id === id);
-      console.log("selectedIngredient", selectedIngredient);
-      const separatedSymbols = selectedIngredient.name.split(",")[0];
-      console.log("separatedSymbols", separatedSymbols);
-      const cleanedIngredient = separatedSymbols.replace(
-        /[\p{Emoji_Presentation}\p{Emoji}\p{Emoji_Modifier_Base}\p{Emoji_Modifier}\p{Emoji_Component}]/gu,
-        ""
-      );
-      console.log("item selected", cleanedIngredient);
-      setSelectedItem(cleanedIngredient);
+    const handleAddItem = newItem => {
+        setItems(prevItems => [...prevItems, newItem]);
     };
 
     return (
-      <main className="container mx-auto p-4 flex">
-          <div className="w-1/2">
-              <h1 className="text-5xl font-bold mb-4 text-center" style={{ fontFamily: 'Indie Flower', color: 'lightgray' }}>Shopping List</h1>
-              <NewItem onAddItem={handleAddItem} />
-              <ItemList items={items} onItemSelect={handleItemSelect} />
-          </div>
-          <div className="w-1/2">
-              <MealIdeas ingredient={selectedItemName} />
-          </div>
-      </main>
-  );
+        <main className="container mx-auto p-4 flex">
+            <div className="flex-1">
+                <h1 className="text-5xl font-bold mb-4 text-center" style={{ fontFamily: 'Indie Flower', color: 'lightgray' }}>Shopping List</h1>
+                <NewItem onAddItem={handleAddItem} />
+                <ItemList items={items} onItemSelect={handleItemSelect} />
+            </div>
+            <div className="flex-1">
+                <MealIdeas ingredient={selectedItemName} />
+            </div>
+        </main>
+    );
 };
+
+export default Page;
