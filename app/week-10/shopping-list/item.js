3b2c7105f0815997f4b60f5181e8ea15e8b0470d
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-27 20:10:36 -0600

    Done

--- app/week-10/shopping-list/item.js
+++ app/week-10/shopping-list/item.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
-const Item = ({ id, name, quantity, category, onSelect, onDelete }) => {
-    const handleDelete = (event) => {
-        event.stopPropagation(); // Prevents the click event from bubbling up to the parent element
-        onDelete(id);
-    };
-
+const Item = ({ id, name, quantity, category, onSelect}) => {
     return (
         <li
             className="border border-gray-300 p-4 m-4 bg-gray-200 text-black flex flex-col flex-grow cursor-pointer relative" // Added relative positioning
@@ -17,9 +12,6 @@ const Item = ({ id, name, quantity, category, onSelect, onDelete }) => {
                     <h3 className="text-lg font-Indie Flower capitalize">{name}</h3>
                     <p>Quantity: {quantity}</p>
                     <p className="capitalize">Category: {category}</p>
-                </div>
-                <div className="absolute top-0 right-0"> {/* Added absolute positioning */}
-                    <button onClick={handleDelete}>Delete</button>
                 </div>
                 <div>
                     <input type="checkbox" style={{ transform: 'scale(1.5)' }} />
--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import ItemList from './item-list.js';
 import NewItem from './new-item.js';
 import MealIdeas from './meal-ideas.js';
-import { getItems, addItem, deleteItem } from '../_services/shopping-list-service.js';
+import { getItems, addItem} from '../_services/shopping-list-service.js';
 import { useUserAuth } from '../_utils/auth-context.js';
 
 const Page = () => {
@@ -41,29 +41,12 @@ const Page = () => {
             console.error('Error adding item:', error);
         }
     };
-
-    const handleDeleteItem = async (itemId) => {
-        // Check if user is defined
-        if (user) {
-            try {
-                const userId = user.uid;
-                await deleteItem(userId, itemId);
-                setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
-            } catch (error) {
-                console.error('Error deleting item:', error);
-            }
-        } else {
-            console.error('User is not defined');
-        }
-    };
-    
-
     return (
         <main className="container mx-auto p-4 flex">
             <div className="flex-1">
                 <h1 className="text-5xl font-bold mb-4 text-center" style={{ fontFamily: 'Indie Flower', color: 'lightgray' }}>Shopping List</h1>
                 <NewItem onAddItem={handleAddItem} />
-                <ItemList items={items} onItemSelect={handleItemSelect} onDeleteItem={handleDeleteItem} />
+                <ItemList items={items} onItemSelect={handleItemSelect}/>
             </div>
             <div className="flex-1">
                 <MealIdeas ingredient={selectedItemName} />
