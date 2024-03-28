9b049b4e96808fca5ea68090fef839faff3dc1bb
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-27 18:01:47 -0600

    Dont Break

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,35 +1,23 @@
-
 "use client";
-
 import React, { useState } from 'react';
 import ItemList from './item-list.js';
 import NewItem from './new-item.js';
 import MealIdeas from './meal-ideas.js';
 
-
-<<<<<<< HEAD
-    const handleItemSelect = (itemName) => {
-=======
 const Page = () => {
-export default function Page() {
->>>>>>> 41d48c31c495c0440c22af51fce5f810e39517fa
+    const [items, setItems] = useState([]);
+    const [selectedItemName, setSelectedItemName] = useState(null);
+
+    const handleItemSelect = (itemName) => {
         // Clean up item name if necessary
         const cleanedItemName = itemName.replace(/,.*|(\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF]|\uD83E[\uDD00-\uDDFF]|[\u2700-\u27BF])/g, '').trim();
         setSelectedItemName(cleanedItemName);
     };
-<<<<<<< HEAD
-    
-  
 
-=======
     const handleAddItem = newItem => {
         setItems(prevItems => [...prevItems, newItem]);
     };
-      // Clean up item name by removing emojis and extra characters
-      const cleanedItemName = itemName.replace(/[\p{Emoji_Presentation}\p{Emoji}\p{Emoji_Modifier_Base}\p{Emoji_Modifier}\p{Emoji_Component}]/gu, '').trim();
-      setSelectedItemName(cleanedItemName);
-  };
->>>>>>> 41d48c31c495c0440c22af51fce5f810e39517fa
+
     return (
         <main className="container mx-auto p-4 flex">
             <div className="flex-1">
@@ -42,5 +30,6 @@ export default function Page() {
             </div>
         </main>
     );
-export default Page;
+};
 
+export default Page;
